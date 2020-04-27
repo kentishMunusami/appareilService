b0e7a29bf614c7a53249f25804121721c428d8e0
kenti <kentish.munusami@gmail.com>
2020-04-27 12:04:25 +0200

    selenium test

--- src/test/java/com/home/appareil/SeleniumTest.java
+++ src/test/java/com/home/appareil/SeleniumTest.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CreationAppareilTest {
+public class SeleniumTest {
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\kenti\\Documents\\dev\\chromedriver_win32\\chromedriver.exe");
